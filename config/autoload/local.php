0ab0022bcff7dd171816dbe40fa046f4ef08cbbc
Charlotte Tse <charlottetse33@gmail.com>
2023-05-03 21:21:07 +1000

    Update local.php

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -6,7 +6,11 @@
  * and provide the email address the system should use.
  */
 
-return array(
+$detailsClass = 'Application\Model\PaymentDetails';
+
+$project_config = require 'config/autoload/project.php';
+
+return array_merge($project_config, array(
     'db' => array(
         'database' => 'payment',
         'username' => 'root',
@@ -16,8 +20,8 @@ return array(
         'port' => null,
     ),
     'mail' => array(
-        'type' => 'sendmail', // or 'smtp' or 'smtp-tls' (or 'file', to not send, but save to file (data/mails/))
-        'address' => 'info@bookings.example.com',
+        'type' => 'sendmail', // or 'smtp' or 'smtp-tls'
+        'address' => '?',
             // Make sure 'bookings.example.com' matches the hosting domain when using type 'sendmail'
 
         'host' => '?', // for 'smtp' type only, otherwise remove or leave as is
@@ -37,11 +41,43 @@ return array(
             // 'hu-HU' => 'Magyar',
         ),
 
-        'currency' => 'EUR',
+        'currency' => 'AUD',
 
         // The language is usually detected from the user's web browser.
         // If it cannot be detected automatically and there is no cookie from a manual language selection,
         // the following locale will be used as the default "fallback":
-        'locale' => 'de-DE',
+        'locale' => 'en-US',
     ),
-);
+    'payum' => array(
+        'token_storage' => new \Payum\Core\Storage\FilesystemStorage(
+            __DIR__.'/../../data/payum',
+            'Application\Model\PaymentSecurityToken',
+            'hash'
+        ),
+        'gateways' => array(
+            'paypal_ec' => (new \Payum\Paypal\ExpressCheckout\Nvp\PaypalExpressCheckoutGatewayFactory())->create(array(
+                'username' => '?',
+                'password' => '?',
+                'signature' => '?',
+                'sandbox' => true
+            )),
+            'stripe' => (new \Payum\Stripe\StripeCheckoutGatewayFactory())->create(array(
+                'publishable_key' => 'pk_test_51N3B1LFxrkun3mFr4l4jqlCMCVaQQBOEaSs389IHa6nNzoUa4S8xekbvdnrM95bxPf9305v2HSGXf4jSUItSTdSj004fHTTvr9',
+                'secret_key' => 'sk_test_51N3B1LFxrkun3mFrYa9JoEAMFrlYBLNULmlJzJBNRtY6l76csZP4yhmO2IoXmJYeCgRDUuUUcOUcTrUl9tzzFmwK00t9W7IY8x',
+                'sca_flow' => true,
+                'payum.template.obtain_token' => '@PayumStripe/Action/stripe_js.html.twig',
+                'payum.template.require_confirmation' => '@PayumStripe/Action/stripe_confirm.html.twig'
+            )),
+            // replace below part with Omnipay 
+			'klarna_checkout' => (new \Payum\Klarna\Checkout\KlarnaCheckoutGatewayFactory())->create(array(
+                'secret' => '?',
+                'merchant_id' => '?',
+                'sandbox' => true
+            )),
+        ),
+        'storages' => array(
+            $detailsClass => new \Payum\Core\Storage\FilesystemStorage(__DIR__.'/../../data/payum', $detailsClass, 'id'),
+        )
+    ),
+));
+
